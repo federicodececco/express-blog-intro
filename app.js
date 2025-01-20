f3303400a7c654bb4849488a051173782a43eb0a
federicodececco <federicodececco1@gmail.com>
2025-01-20 13:22:04 +0100

    fix:img not showing

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ app.get("/", (req, res) => {
   res.send("Server del mio blog");
 });
 app.get("/bacheca", (req, res) => {
-  const ciao = [
-    { title: "Ciambellone", content: "Tante cose belle", img: "/images/ciambellone.jpeg", tags: ["torta"] },
-    { title: "Cracker alla Barbabietola", content: "Un po' di cose belle", img: "/images/creacker_barbabietola.jpeg", tags: ["pasta", "pizza","mandolino"] },
-    { title: "Pane firtto dolce", content: "Nessuna cosa bella", img: "/images/pane_fritto_dolce.jpeg", tags: ["pane", "zucchero"] },
-    { title: "Pasta alla Barbabietola", content: "Some cose belle", img: "/images/pasta_barbabietola.jpeg", tags: ["barbabietola", "pasta"] },
-    { title: "Torta Paesana", content: "Any belle cose", img: "/images/torta_paesana.jpeg", tags: ["torta"] },
-  ];
-  res.json(ciao);
+
+  res.json([
+    { title: "Ciambellone", content: "Tante cose belle", img: `http://localhost:${port}/images/ciambellone.jpeg`, tags: ["torta"] },
+    { title: "Cracker alla Barbabietola", content: "Un po' di cose belle", img: `http://localhost:${port}/images/creacker_barbabietola.jpeg`, tags: ["pasta", "pizza","mandolino"] },
+    { title: "Pane firtto dolce", content: "Nessuna cosa bella", img: `http://localhost:${port}/images/pane_fritto_dolce.jpeg`, tags: ["pane", "zucchero"] },
+    { title: "Pasta alla Barbabietola", content: "Some cose belle", img: `http://localhost:${port}/images/pasta_barbabietola.jpeg`, tags: ["barbabietola", "pasta"] },
+    { title: "Torta Paesana", content: "Any belle cose", img:  `http://localhost:${port}/images/torta_paesana.jpeg`, tags: ["torta"] },
+  ]);
 });
 app.listen(port, () => {});
